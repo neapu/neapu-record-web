8737d625f7fa7fb2b54ab0668bc2f7c8e087b9d5
neapu <neapu@neapu.com>
2025-01-25 16:59:52 +0800

    补全基本功能

--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -2,6 +2,8 @@
 // import {ref} from "vue";
 import {useDataStore} from "../stores/data.ts";
 import {onBeforeMount} from "vue";
+import axios from "axios";
+import { ElNotification, ElMessageBox } from 'element-plus';
 
 const dataStore = useDataStore();
 
@@ -11,12 +13,92 @@ onBeforeMount(() => {
   dataStore.updateRooms();
 });
 
-function onRemoveRoomClicked(_roomId: number) {
-  // TODO
+function onRemoveRoomClicked(roomId: number) {
+  ElMessageBox.confirm('确定删除该房间吗？', '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning',
+  }).then(() => {
+    onStartRemoveRoom(roomId);
+  }).catch(() => {
+    // do nothing
+  });
+}
+
+async function onStartRemoveRoom(roomId: number) {
+  try {
+    const ret = await axios.post('/api/room/deleteRoom', {roomId: roomId});
+    const {code, data} = ret.data;
+    if (code === 0) {
+      ElNotification({
+        title: '删除成功',
+        message: '删除成功',
+        type: 'success',
+      });
+      dataStore.updateRooms();
+    } else {
+      console.error(data);
+      ElNotification({
+        title: '删除失败',
+        message: data,
+        type: 'error',
+      });
+    }
+  } catch (e: any) {
+    console.error(e);
+    ElNotification({
+      title: '删除失败',
+      message: e.message.toString(),
+      type: 'error',
+    });
+  }
 }
 
 function onListenStatusChange(_roomId: number, _listening: boolean) {
-  // TODO
+  let text = "";
+  if (_listening) {
+    text = "是否取消监听？";
+  } else {
+    text = "是否启动监听？";
+  }
+  ElMessageBox.confirm(text, '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning',
+  }).then(() => {
+    startListenStatusChange(_roomId, _listening);
+  }).catch(() => {
+    // do nothing
+  });
+}
+
+async function startListenStatusChange(_roomId: number, _listening: boolean) {
+  try {
+    const ret = await axios.post('/api/room/setRoomListenStatus', {roomId: _roomId, listening: !_listening});
+    const {code, data} = ret.data;
+    if (code === 0) {
+      ElNotification({
+        title: '操作成功',
+        message: '操作成功',
+        type: 'success',
+      });
+      dataStore.updateRooms();
+    } else {
+      console.error(data);
+      ElNotification({
+        title: '操作失败',
+        message: data,
+        type: 'error',
+      });
+    }
+  } catch (e: any) {
+    console.error(e);
+    ElNotification({
+      title: '操作失败',
+      message: e.message.toString(),
+      type: 'error',
+    });
+  }
 }
 </script>
 
